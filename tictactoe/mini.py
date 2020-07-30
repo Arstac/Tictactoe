80644989bf2644d39166e868de0a7907d6e23137
unknown <arnaucosta95@gmail.com>
2020-07-30 17:39:56 +0200

    tictactoe i mini funcionant, queden 2 funcionas a desenvolupar

--- tictactoe/mini.py
+++ tictactoe/mini.py
@@ -9,28 +9,30 @@ EMPTY = None
 
 player_turn = 1
 
-board= [[X, X, O],
-    [O, EMPTY, EMPTY],
-    [EMPTY, EMPTY, EMPTY]]
+board= [[O, X, X],
+    [X, O, O],
+    [X, O, X]]
 
 def player(board):
-	counter = 0
-	for i in board:
-		for j in i:
-			if j is None:
-				counter += 1
+    counter = 0
+    for i in board:
+        for j in i:
+            if j is None:
+                counter += 1
+    print (counter)
+    #si el numero no te residu (es parell), i no es igual a 0 (que significa que no hi han moviments):
+    if (counter % 2 ==0 and counter != 0):
+        return O
+    #si es 0 vol dir que no hi han moviments, n oes el torn de ningu i retorno None
+    elif (counter == 0):
+        return EMPTY
 
-	if (counter % 2 ==0):
-		return O
-	elif (counter == 0):
-		return EMPTY
-
-	else:
-		return X
+    else:
+        return X
 
 
-	print (counter)
-	print (player_turn)
+    print (counter)
+    print (player_turn)
 
 
 def actions(board):
@@ -50,13 +52,76 @@ def actions(board):
     return (possible_actions)
 
 
-action=(1,1)
+def winner(board):
+    """
+    Returns the winner of the game, if there is one.
+    
+    If the X player has on the game, function should return x. Same for O
 
-if action in actions(board):
-    new_board = copy.deepcopy(board)
-    print (new_board)
-    new_board[action[0]][action[1]]=player(board)
-    print (new_board)
+    One can win if three of their moves ina row horizontally, vertically, diagonally.
 
-else:
-    raise Exception("This action cannot be implemented in this board")
+    Assume that there will be AT MOST one winner
+
+    If there is no winner function should return None
+    """
+    win_player = EMPTY
+    #un altre metode per comprovar tres en rlla horitzontal, pero agafo l'altre aixi horitzontla i vertical son el mateix
+    # for i in board:
+    #     check = i[0]
+    #     for j in i:
+    #         if j == check:
+    #             print ("Same as before, keep going")
+    #             print j
+    #         else return None
+    #comprovo si hi ha 3 en ralla HORITZONTAL
+    for i in range(0,2):
+        if board[i][0] == board[i][1]:
+            if board[i][0] == board[i][2]:
+                win_player = board[i][0]
+                print(f"tres en ralla horitzonal de {win_player}")
+                return win_player       
+        else:
+            win_player = EMPTY
+
+
+    #comprovo si hi ha 3 en ralla VERTICAUl (ve a ser el mateix que horitzonal tot ique primer miro les j)
+    for i in range(0,2):
+        if board[0][i] == board[1][i]:
+            if board[0][i] == board[2][i]:
+                win_player = board[0][i]
+                print(f"tres en ralla vertical de {win_player}")
+                
+                return win_player
+        else:
+            win_player = EMPTY
+
+
+    #comprovo si hi ha 3 en ralla DIAGONAL
+    if board[0][0] == board[1][1] and  board[0][0] == board[2][2]:
+        win_player = board[0][0]
+        print (f"Tres en ralla diagonal <- de {win_player}")
+        return win_player
+    elif board[0][2] == board[1][1] and  board[0][2] == board[2][0]:
+        win_player = board[0][2]
+        print (f"Tres en ralla diagonal -> de {win_player}")
+        return win_player
+    else:
+        win_player = EMPTY
+
+    if win_player == None:
+        print ("Nobody wins")
+        return None
+
+def terminal(board):
+    """
+    Returns True if game is over, False otherwise.
+    It will return True if there aren't moves to do in the board or if someone has win
+    """
+    #If there are no moves:
+    if player(board) == None or winner(board) == X or winner(board) == O:
+        print("no moves or win")
+        return True
+    else:
+        return False
+
+terminal(board)
--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -33,15 +33,19 @@ def player(board):
             0 EMPTYS -> Game is over
 
     """
+    counter = 0
+
     for i in board:
     for j in i:
         if j is None:
             counter += 1
-
-    if (counter % 2 ==0):
+    #si el numero no te residu (es parell), i no es igual a 0 (que significa que no hi han moviments):
+    if (counter%2 == 0 and counter != 0):
         return O
+    #si es 0 vol dir que no hi han moviments, n oes el torn de ningu i retorno None
     elif (counter == 0):
         return None
+    #si no es cap cas anterior, nomes pot ser el torn de X
     else:
         return X
 
@@ -96,12 +100,66 @@ def result(board, action):
 
     
 
-
 def winner(board):
     """
     Returns the winner of the game, if there is one.
+    
+    If the X player has on the game, function should return x. Same for O
+
+    One can win if three of their moves ina row horizontally, vertically, diagonally.
+
+    Assume that there will be AT MOST one winner
+
+    If there is no winner function should return None
     """
-    raise NotImplementedError
+    win_player = EMPTY
+    #un altre metode per comprovar tres en rlla horitzontal, pero agafo l'altre aixi horitzontla i vertical son el mateix
+    # for i in board:
+    #     check = i[0]
+    #     for j in i:
+    #         if j == check:
+    #             print ("Same as before, keep going")
+    #             print j
+    #         else return None
+    #comprovo si hi ha 3 en ralla HORITZONTAL
+    for i in range(0,2):
+        if board[i][0] == board[i][1]:
+            if board[i][0] == board[i][2]:
+                win_player = board[i][0]
+                print(f"tres en ralla horitzonal de {win_player}")
+                return win_player       
+        else:
+            win_player = EMPTY
+
+
+    #comprovo si hi ha 3 en ralla VERTICAUl (ve a ser el mateix que horitzonal tot ique primer miro les j)
+    for i in range(0,2):
+        if board[0][i] == board[1][i]:
+            if board[0][i] == board[2][i]:
+                win_player = board[0][i]
+                print(f"tres en ralla vertical de {win_player}")
+                
+                return win_player
+        else:
+            win_player = EMPTY
+
+
+    #comprovo si hi ha 3 en ralla DIAGONAL
+    if board[0][0] == board[1][1] and  board[0][0] == board[2][2]:
+        win_player = board[0][0]
+        print (f"Tres en ralla diagonal <- de {win_player}")
+        return win_player
+    elif board[0][2] == board[1][1] and  board[0][2] == board[2][0]:
+        win_player = board[0][2]
+        print (f"Tres en ralla diagonal -> de {win_player}")
+        return win_player
+    else:
+        win_player = EMPTY
+
+    if win_player == None:
+        print ("Nobody wins")
+        return None
+
 
 
 def terminal(board):
@@ -110,7 +168,7 @@ def terminal(board):
     It will return True if there aren't moves to do in the board or if someone has win
     """
     #If there are no moves:
-    if player(board) == None or ##############:
+    if player(board) == None or winner(board) == X or winner(board) == O
         return True
     else False
 
